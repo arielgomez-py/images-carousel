dd5c083ceb2eb4ed775b114a5a0146db3a3fc282
Ariel <arielgomezmendez94@gmail.com>
2023-02-05 11:56:08 -0500

    Create style file Button.css

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,13 +1,10 @@
 import React from "react";
+import "./Button.css";
 
 const Button = (props) => {
   return (
     <div>
-      <button
-        onClick={props.onClick}
-      >
-        {props.span}
-      </button>
+      <button onClick={props.onClick}>{props.span}</button>
     </div>
   );
 };
